5dc634f9469be2040113bfd603ec77bb5a91d645
mokoko <guswlso2@naver.com>
2025-02-18 19:52:29 +0900

    feat: 폰트 pretendard 설정정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { pretendard } from '@/lib/font';
 
 export const metadata: Metadata = {
   title: 'Coworkers',
@@ -23,12 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='ko' className={pretendard.variable}>
+      <body className={'antialiased'}>{children}</body>
     </html>
   );
 }
